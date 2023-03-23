4a13ad4294933e2c45da8184728b6c4718074623
Surag2001 <22C029@gectcr.ac.in>
2023-03-23 15:44:55 +0530

    2. co2 exp3 commit 1

--- StringMethods.java
+++ StringMethods.java
@@ -1,22 +1,56 @@
 import java.util.Scanner;
-//https://www.geeksforgeeks.org/string-class-in-java/
+
 public class StringMethods {
     static String string;
 
     static String answer(int option) {
-        String s;
+        String s, str;
         Scanner sc = new Scanner(System.in);
+        int index;
 
         switch(option) {
-            case 1: s = "The number of characters in the string is " + string.length();
-                    break;
-            case 2: System.out.print("Input index:\t");
-                    int index = sc.nextInt();
-                    s = "The character at index " + index + " is " + string.charAt(index);
-                    break;
-            
-
-
+            case 1: 	s = "The number of characters in the string is " + string.length();
+                    	break;
+            case 2: 	System.out.println("Input index:");
+                    	index = sc.nextInt();
+                    	s = "The character at index " + index + " is " + string.charAt(index);
+                    	break;
+            case 3: 	System.out.println("Input index:");
+            			index = sc.nextInt();
+            			s = "The substring from " + index + "th index character is " + string.substring(index);
+            			break;
+            case 4: 	System.out.println("Input starting and ending index:");
+            			int index1 = sc.nextInt(), index2 = sc.nextInt();
+            			s = "The substring from index " + index1 + " to index " + index2 + " is " + string.substring(index1, index2); 
+            			break;
+            case 5: 	System.out.println("Input a string:");
+            			str = sc.next();
+            			s = "The concatenated string is " + string.concat(str);
+            			break;
+            case 6: 	System.out.println("Input a string:");
+    					str = sc.next();
+    					s = "The index of first occurrence of " + str + " in " + string + " is " + string.indexOf(str);
+    					break;
+            case 7: 	System.out.println("Input a string:");
+						str = sc.next();
+						System.out.println("Input a starting index:");
+						index = sc.nextInt();
+						s = "The index of first occurrence of " + str + " in " + string + " starting from index " + index + " is " + string.indexOf(str, index);
+						break;
+            case 8: 	System.out.println("Input a string:");
+						str = sc.next();
+						s = "The index of last occurrence of " + str + " in " + string + " is " + string.lastIndexOf(str);
+						break;
+            case 9: 	s = "The lower case form of " + string + " is " + string.toLowerCase();
+            			break;
+            case 10:	s = "The upper case form of " + string + " is " + string.toUpperCase();
+            			break;
+            case 11:	s = "The trimmed version of " + string + " is " + string.trim();
+            			break;
+            case 12:	System.out.println("Enter 2 characters");
+            			char ch1 = sc.next().charAt(0), ch2 = sc.next().charAt(0);
+            			s = "The new string replacing all occurrences of \'" + ch1 + "\' with \'" + ch2 + "\' in " + string + " is " + string.replace(ch1, ch2);
+            			break;
             default: return "The string without any manipulation is " + string;
         }
 
@@ -27,7 +61,7 @@ public class StringMethods {
         System.out.println("Input a string");
         string = sc.nextLine();
         
-        System.out.println("The major string methods are:");
+        System.out.println("\nThe major string methods are:");
         System.out.println("1.  int length()");
         System.out.println("2.  Char charAt(int i)");
         System.out.println("3.  String substring (int i)");
@@ -36,14 +70,10 @@ public class StringMethods {
         System.out.println("6.  int indexOf (String s)");
         System.out.println("7.  int indexOf (String s, int i)");
         System.out.println("8.  int lastIndexOf( String s)");
-        System.out.println("9.  boolean equals( Object otherObj)");
-        System.out.println("10. boolean  equalsIgnoreCase (String anotherString)");
-        System.out.println("11. int compareTo( String anotherString)");
-        System.out.println("12. int compareToIgnoreCase( String anotherString)");
-        System.out.println("13. String toLowerCase()");
-        System.out.println("14. String toUpperCase()");
-        System.out.println("15. String trim()");
-        System.out.println("16. String replace (char oldChar, char newChar)");
+        System.out.println("9.  String toLowerCase()");
+        System.out.println("10. String toUpperCase()");
+        System.out.println("11. String trim()");
+        System.out.println("12. String replace (char oldChar, char newChar)");
 
         do {
             System.out.println("\nEnter an option (1 to 16) [Input any other no. to exit]");
